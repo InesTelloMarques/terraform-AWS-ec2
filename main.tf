4ef2b68a5009ce087b866cd289898ad34d218d4c
InesTelloMarques <173058694+InesTelloMarques@users.noreply.github.com>
2024-07-03 12:47:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,22 @@ terraform {
     }
   }
 }
+
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-west-2" 
 }
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.1"
-}
+
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tf-example"
+    Name = var.vpc_name
   }
 }
+
+variable "vpc_name" {}
+variable "instance_name" {}
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
@@ -45,6 +46,10 @@ resource "aws_instance" "foo" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
 
+  tags = {
+    Name = var.instance_name
+  }
+
   network_interface {
     network_interface_id = aws_network_interface.foo.id
     device_index         = 0
@@ -52,5 +57,8 @@ resource "aws_instance" "foo" {
 
   credit_specification {
     cpu_credits = "unlimited"
+  }
+ tags = {
+    Name = "primary_network_interface"
   }
 }
